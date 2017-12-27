ffcc31c5e4c6443f639b2a2dcfb45b0a21c95c62
Nisa Olcay <nisaolcayy@gmail.com>
2017-12-27 20:23:07 +0300

    Fixed a lot of bugs

--- cowsnbulls.py
+++ cowsnbulls.py
@@ -5,72 +5,64 @@ print ('\n'' \n'
 		"******************************************************************************"'\n'
 		"Hello! Welcome to the Cows & Bulls game!"'\n'
 		"Try to guess the number I'm thinking of with n digits."'\n'
-		"When one of your digit is in the same place as mine I will count it as a Cow."'\n'
-		"If the digit is right but it is place is wrong, I will count it as a Bull."'\n'
-		"Good Luck!"'\n'
+		"When one of your digit is in the same place as mine I will count it as a 'Bull'."'\n'
+		"If the digit is right but it's in the wrong place, I will count it as a 'Cow'."'\n'
+		"Good Luck!\n"
+		"If you don't want to play you can always enter the word 'exit'.\n"
 		"******************************************************************************"
 		'\n''\n')
 
-n = ""
-
 while True:
-	n = raw_input("\nTo make it easy for you, you can choose the digit number, just type in below :)\n\n")
-	if n.isdigit():
-		n = int(n)
-		break
+    n = raw_input("\nTo make it easy for you, you can choose the digit number, just type in below :)\n")
+    if str(n).lower() == "exit":
+        print("\nYou could've at least try it..")
+        sys.exit()
+    if n.isdigit() == False:
+        print("Please enter a digit\n")
+    else:
+        n = int(n)
+        break
 
-if str(n).lower() == "exit":
-	print '\n'"We were having fun, goodbye :("
-	sys.exit(0)
+def generateRandomNumber(digit):
+    range_start = 10**(digit-1)
+    range_end = 10**digit
+    return randint(range_start, range_end)
+    
+RandomNumber = generateRandomNumber(n)
 
-PlayerNumber = []
-RandomNumber = []
-def RandomNumber (n):
-	range_start = 10**(n - 1)
-	range_end = (10 ** n)-1
-	return randint(range_start, range_end)
-
-RandomNumber = "123"  #str(RandomNumber(n))
-
-cows = 0
-bulls = 0
 guesses = 0
+a = 0
+while  0 < n:
+    PlayerNumber = raw_input("\nPlease enter your guess below.\n")
+    if str(PlayerNumber).lower() == "exit":
+        print("\nWe we're having fun :( Goodbye :( ")
+        sys.exit()
+    if PlayerNumber.isdigit() == False or len(str(RandomNumber)) != len(str(PlayerNumber)):
+        print("\nenter a number with " +str(n) +" digits")
+    else:
+        cows = 0
+        bulls = 0
+        
+        RandomNumber_digits = list(str(RandomNumber))
+        PlayerNumber_digits = list(str(PlayerNumber))
 
-
-while cows < n:
-	PlayerNumber = raw_input("\nPlease enter your guess below. \nEnter 'exit' to quit the game.\n\n" )
-	if str(PlayerNumber).lower() == "exit":
-		print '\n'"We we're having fun :( Goodbye :( "
-		break
-	if len(PlayerNumber) == len(RandomNumber):
-		if PlayerNumber.isdigit():
-
-			cows = 0
-			bulls = 0
-			for i in range (0,n):
-				if PlayerNumber[i] == RandomNumber[i]:
-					cows += 1
-				elif PlayerNumber[i] in RandomNumber[-1 * (n-i)]:
-                                        bulls += 1
-
-			#bulls = bulls - cows
-
-			print ('\n'"You have " + str(cows) + " Cows and " + str(bulls) + " Bulls"'\n')
-			guesses += 1
-
-		else:
-			print "Please enter only numbers."
-
-	else:
-		print ("\nPlese enter " + str(n) + " digits of numbers.")
-
-
-if cows == n:
-	if guesses > 3:
-		print ('\n'"Congratilations!\nYou have found the number after " + str(guesses) + " guesses.\n"
-				"Better luck next time!")
-	else:
-		print ('\n'"Congratilations!\nYou have found the number after " + str(guesses) + " guess" + ("." if guesses == 1 else "es.") +
+        for i in range(n):    
+            if PlayerNumber_digits[i] == RandomNumber_digits[i]:
+                bulls += 1
+                RandomNumber_digits[i] = -1
+                PlayerNumber_digits[i] = -2
+        for i in range(n):
+            if PlayerNumber_digits[i] in RandomNumber_digits:
+                cows += 1  
+        print("\nYou have " + str(bulls) + " Bulls and " + str(cows) + " Cows\n")
+    
+        guesses += 1
+        
+        if bulls == n:
+            if guesses > n:
+                print ("\nCongratilations!\nYou have found the number after " +str(guesses) + " guesses.\nBetter luck next time :P")
+            else:
+		        print ('\n'"Congratilations!\nYou have found the number after " + str(guesses) + " guess" + ("." if guesses == 1 else "es.") +
 		       "\nYou were better then I expected...")
-
-
+            sys.exit()
+            
